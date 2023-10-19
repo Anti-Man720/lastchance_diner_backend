2ad33e29688b5f8a6522b073e5e183f354d06781
M~dot~Carter <120771453+Anti-Man720@users.noreply.github.com>
2023-10-19 05:20:47 -0500

    server and data config update

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express'
-import cors from 'cors'
+import { fileURLToPath } from 'url';
+import express from 'express';
+import cors from 'cors';
 import foodRouter from './routers/food.router.js';
 import userRouter from './routers/user.router.js';
-
+import orderRouter from './routers/order.router.js';
 
 import { dbconnect } from './config/database.config.js';
+import path, { dirname } from 'path';
 dbconnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
@@ -23,6 +28,15 @@ app.use(
 
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
+
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
+});
 
 
 const port = process.env.PORT || 5000;
