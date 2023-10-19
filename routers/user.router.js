fdaca18672d4e2ee0aa430c21ff17f1b1f0f42da
M~dot~Carter <120771453+Anti-Man720@users.noreply.github.com>
2023-10-19 00:43:26 -0500

    updated user router(midware and httpStatus error)

--- routers/user.router.js
+++ routers/user.router.js
@@ -5,6 +5,7 @@ import { BAD_REQUEST } from '../constants/httpStatus.js';
 import handler from 'express-async-handler';
 import { UserModel } from '../Models/user.model.js';
 import bcrypt from 'bcryptjs';
+import auth from '../middleware/auth.mid.js';
 const PASSWORD_HASH_SALT_ROUNDS = 10;
 
 router.post(
@@ -12,10 +13,12 @@ router.post(
   handler(async (req, res) => {
     const { email, password } = req.body;
     const user = await UserModel.findOne({ email });
+
     if (user && (await bcrypt.compare(password, user.password))) {
       res.send(generateTokenResponse(user));
       return;
     }
+
     res.status(BAD_REQUEST).send('Username or password is invalid');
   })
 );
@@ -38,14 +41,55 @@ router.post(
     );
 
     const newUser = {
-      name: name,
+      name,
       email: email.toLowerCase(),
       password: hashedPassword,
-      address: address,
+      address,
     };
 
     const result = await UserModel.create(newUser);
     res.send(generateTokenResponse(result));
+  })
+);
+
+router.put(
+  '/updateProfile',
+  auth,
+  handler(async (req, res) => {
+    const { name, address } = req.body;
+    const user = await UserModel.findByIdAndUpdate(
+      req.user.id,
+      { name, address },
+      { new: true }
+    );
+
+    res.send(generateTokenResponse(user));
+  })
+);
+
+router.put(
+  '/changePassword',
+  auth,
+  handler(async (req, res) => {
+    const { currentPassword, newPassword } = req.body;
+    const user = await UserModel.findById(req.user.id);
+
+    if (!user) {
+      res.status(BAD_REQUEST).send('Change Password Failed!');
+      return;
+    }
+
+    const equal = await bcrypt.compare(currentPassword, user.password);
+
+    if (!equal) {
+      res.status(BAD_REQUEST).send('Current Password Is Not Correct!');
+      return;
+    }
+
+    user.password = await bcrypt.hash(newPassword, PASSWORD_HASH_SALT_ROUNDS);
+    await user.save();
+
+    res.send();
   })
 );
 
@@ -61,18 +105,17 @@ const generateTokenResponse = user => {
       expiresIn: '30d',
     }
   );
+
   return {
     id: user.id,
     email: user.email,
     name: user.name,
     address: user.address,
-    city: user.city,
-    state: user.state,
-    zip: user.zip,
     isAdmin: user.isAdmin,
     token,
   };
 };
+
 export default router;
 // Path: backend/src/routers/user.router.js
 
